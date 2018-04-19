daf7ff5621e503d21281ed9e33a4553f63790025
Daniel Shaw <dls1992@live.co.uk>
2018-04-19 19:56:49 +0100

    Long press to engage negative mode, which subtracts

--- score-keeper/src/score_keeper.js
+++ score-keeper/src/score_keeper.js
@@ -14,11 +14,17 @@ class Players extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: [{ value: 9 }, { value: 99 }, { value: 999 }, { value: 9999 }, { value: 99999 }]
+      players: [{ value: 9 }, { value: 99 }, { value: 999 }, { value: 9999 }, { value: 99999 }],
+      increment: 1,
+      buttonPressTimer: 0,
+      reverseModeWait: 1000
     };
+    this.reverseMode = this.reverseMode.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
   }
   render() {
-    var playerList = []
+    var playerList = [];
     for (var i = 0; i < this.state.players.length; i++) {
       playerList.push(this.renderPlayer(i));
     }
@@ -32,24 +38,44 @@ class Players extends Component {
 
   renderPlayer(i) {
     return (
-      <Player value={this.state.players[i].value} onClick={() => this.handleClick(i)} />
+      <Player key={i.toString()} value={this.state.players[i].value} onClick={() => this.handleClick(i)} onmousedown={() => this.handleMouseDown()} onmouseup={() => this.handleMouseUp()} />
     )
   }
 
   handleClick(i) {
-    const players = this.state.players.slice();
-    players[i].value += 1;
-    this.setState = { players: players }; //always set sat, never assign to this.state
+    const state = this.state;
+    state.players.slice()[i].value += this.state.increment;
+    this.setState(state);; //always set sat, never assign to this.state
     this.forceUpdate(); //this probably can be improved...
   }
+
+  handleMouseDown() {
+    const state = this.state;
+    state.buttonPressTimer = setTimeout(this.reverseMode, this.state.reverseModeWait);
+    this.setState(state);
+  }
+
+
+  handleMouseUp() {
+    const state = this.state;
+    state.buttonPressTimer = clearTimeout(state.buttonPressTimer);
+    this.setState(state);
+  }
+
+  reverseMode() {
+    const state = this.state;
+    state.increment *= -1;
+    this.setState(state);
+  }
+
 }
 
 class Player extends Component {
   render() {
     return (
-      <button className="player" onClick={() => this.props.onClick()} >
+      <button className="player" onClick={() => this.props.onClick()} onMouseDown={() => this.props.onmousedown()} onMouseUp={() => this.props.onmouseup()}>
         {this.props.value}
-      </button>
+      </ button>
     )
   }
 }
