bf953e4c6e7df83addab06831848e563876a487d
Daniel Shaw <dls1992@live.co.uk>
2018-08-17 20:26:26 +0100

    Removed form
    submitting on phone keyboard was undoing edit

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -13,14 +13,14 @@ class EditForm extends Component {
 
     render() {
         return (
-            <form class={this.props.editMode ? "editor enabled" : "editor disabled"}>
+            <div class={this.props.editMode ? "editor enabled" : "editor disabled"}>
                 <label htmlFor={this.props.playerId + "_name"} >Player Name:</label>
                 <input type="text" id={this.props.playerId + "_name"} data-player-id={this.props.playerId} defaultValue={this.props.player.name} onChange={this.handleChange.bind(this)} name="name"></input>
                 <label htmlFor={this.props.playerId + "_colour"} >Colour</label>
                 <input type="color" id={this.props.playerId + "_colour"} data-player-id={this.props.playerId} defaultValue={this.props.player.bgColor} onChange={this.handleChange.bind(this)} name="bgColor" ></input>
                 <label htmlFor={this.props.playerId + "_text"}>Text</label>
                 <input type="color" id={this.props.playerId + "_text"} data-player-id={this.props.playerId} defaultValue={this.props.player.color} onChange={this.handleChange.bind(this)} name="color"></input>
-            </form >
+            </div >
         );
     }
 }
